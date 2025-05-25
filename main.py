2c41fc6163379a3993da911063cea578a362b4f4
Ovi0123 <140853940+Ovi0123@users.noreply.github.com>
2025-05-25 18:56:46 +0530

    Added prometheus in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, session , make_response , send_file , flash
 import os
+# using prometheouis export for monitoring 
+from prometheus_flask_exporter import PrometheusMetrics
 from db.db_connection import connect_to_database
 from werkzeug.utils import secure_filename
 import gridfs
@@ -15,6 +17,8 @@ from user.dashboard import get_user_data, save_user_thought, update_user_details
 
 
 app = Flask(__name__)
+# Exposing the monitoring on /metrics endpoint
+metrics = PrometheusMetrics(app)
 app.secret_key = b'gSnApZbWGl8mZlHFwj_QszpUN06Q0IAjGRXnXZaUYeI='
 
 def load_key():
